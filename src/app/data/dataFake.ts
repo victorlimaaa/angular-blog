90535167853a83776f45515a214723704d81d394
victor <victormanoel119@gmail.com>
2023-10-11 21:03:18 -0300

    criando segunda página e tornando ela dinâmica

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photo": "aaa",
+        "title": "teste",
+        "description": "testando através de dados feique",
+        "photoCover": "https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABclgv7H8H4JR4JRUTZCHWLf9S04YyHSkH2kKPcMpM1Mnya4lxprOIitjLcVWTy8bleORWxJo2kPz2MfD71cSQKO3ZFcmpKg331cP.jpg?r=13a"
+    },
+    {
+        "id": "2",
+        "photo": "aaa",
+        "title": "teste",
+        "description": "testando através de dados feique",
+        "photoCover": ""
+    }
+]
